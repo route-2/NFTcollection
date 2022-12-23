82946d8b7694424bde462da98a78d340d8481619
route-2 <ruthurao@gmail.com>
2022-12-23 23:05:06 +0530

    redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {Provider} from "react-redux"
+import { configureStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
 import App from "./App";
 
 import {ChakraProvider} from "@chakra-ui/react"
+
+import reducers from "./reducers";
+
+
+const store = configureStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
 <ChakraProvider><App /> </ChakraProvider>, document.getElementById("root"));
