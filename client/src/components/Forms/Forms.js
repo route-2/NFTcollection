394961e8fec7f91bbc741a0d37af658ee9a76b36
route-2 <ruthurao@gmail.com>
2022-12-27 19:37:36 +0530

    Form

--- client/src/components/Forms/Forms.js
+++ client/src/components/Forms/Forms.js
@@ -1,9 +1,80 @@
 import React from "react";
-import { Text } from "@chakra-ui/react";
+import { Text,FormControl, Box,Button, Input } from "@chakra-ui/react";
+import { useState } from "react";
+import FileBase from "react-file-base64";
 
 const Form = () => {
-    return (
-        <Text fontSize='2xl' as='b' > Forms </Text>
-    );
-    }
-    export default Form;
+    const [postData, setPostData] = useState({
+        creator: "",
+        title: "",
+        message: "",
+        tags: "",
+        selectedFile: "",
+    });
+
+  const handleSubmit = () => {};
+  const clear = () => {};
+
+  return (
+    <div>
+     <Text fontSize='2xl' as='b' > Create a Post </Text>
+       <form> 
+        <FormControl>
+            <Input
+                type="text"
+                placeholder="Creator"
+                value={postData.creator}
+                onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
+            />
+            </FormControl>
+
+            <FormControl>
+            <Input
+                type="text" 
+                placeholder="Title"
+                value={postData.title}
+                onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+            />
+            </FormControl>
+
+            <FormControl>
+            <Input
+                type="text"
+                placeholder="Message"
+                value={postData.message}
+                onChange={(e) => setPostData({ ...postData, message: e.target.value })}
+            />
+            </FormControl>
+
+            <FormControl>
+            <Input
+                type="text"
+                placeholder="Tags"
+                value={postData.tags}
+                onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+            />
+            </FormControl>
+
+            <FormControl>
+            <div>
+                <FileBase
+                    type="file"
+                    multiple={false}
+                    onDone={({ base64 }) =>
+                        setPostData({ ...postData, selectedFile: base64 })
+                    }
+                />
+            </div>
+            <Button onClick={handleSubmit} colorScheme="teal" variant="outline">
+                Submit
+            </Button>
+            <Button onClick={clear} colorScheme="teal" variant="outline">
+                Clear
+            </Button>
+
+        </FormControl>
+        </form>
+    </div>
+  );
+};
+export default Form;
