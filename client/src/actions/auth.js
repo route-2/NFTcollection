e602922800e860c9520222835164bec0ca19d489
route-2 <ruthurao@gmail.com>
2023-01-11 03:24:12 +0530

    no error but doesn't render :)

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,28 +2,28 @@ import * as api from "../api/index.js";
 
 
 // Action Creators
-export const signin = (formData,router) => {
+export const signin = (formData,navigate) => {
     return async (dispatch) => {
         try {
         // log in the user...
 
         const {data } = await api.signIn(formData);
         dispatch({type: 'AUTH', data});
-        router.push("/");
+        navigate("/");
         } catch (error) {
         console.log(error, "signin action failed");
         }
     };
 }
 
-export const signup = (formData,router) => {
+export const signup = (formData,navigate) => {
     return async (dispatch) => {
         try {
         // sign up the user...
         const {data } = await api.signUp(formData);
         dispatch({type: 'AUTH',  data});
 
-        router.push("/");
+        navigate("/");
         } catch (error) {
         console.log(error, "signup action failed");
         }
