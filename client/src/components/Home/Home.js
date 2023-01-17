76ee3ae68c3038d306a51cf83b02c83674172103
route-2 <ruthurao@gmail.com>
2023-01-18 00:48:37 +0530

    search

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Grid, Text, Button, GridItem, Image } from "@chakra-ui/react";
+import { Box, Grid, Text, Button, GridItem,Input,InputRightElement,InputGroup,InputRightAddon, Image } from "@chakra-ui/react";
 import Pagination from "../Posts/Pagination";
 import Posts from "../Posts/Posts";
 import Form from "../Forms/Forms";
@@ -7,7 +7,12 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { getPosts } from "../../actions/posts";
+import {Search2Icon} from "@chakra-ui/icons"
+import { useLocation } from "react-router-dom";
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
@@ -15,6 +20,11 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(10);
+  const[search,setSearch]=useState([]);
+  const query = useQuery();
+  const[tags,setTags]=useState([]);
+  const searchQuery = query.get('searchQuery');
+  
 
   useEffect(() => {
     dispatch(getPosts());
@@ -30,6 +40,7 @@ const Home = () => {
 
     fetchPosts();
   }, []);
+
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
@@ -37,7 +48,14 @@ const Home = () => {
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
 
+const handlekeyPress = (e) => {
+    const { name, value } = e.target;
+    setPosts({ ...posts, [name]: value });
+  };
+  const handleAddChip = (tag) => setTags([...tags, tag]);
 
+  const handleDeleteChip = (chipToDelete) => setTags(tags.filter((tag) => tag !== chipToDelete));
+ 
   return (
     <Box m={2}>
       <Grid
@@ -52,12 +70,55 @@ const Home = () => {
         </GridItem>
         <GridItem colStart={8} colEnd={8} h="10">
         <div>
+
+        <Box>
+        <Text variant="h6" align="center"
+        name="search"   
+        value={search}           
+         label="search posts"
+         onChange={(e) => setSearch(e.target.value)}>
+
+        
+        
+        
+        </Text>
+        <InputGroup size='sm'>
+   
+    <Input placeholder=' Search ' onAdd={handleAddChip}  onDelete={handleDeleteChip} value={tags} />
+    <InputRightElement children= {<Search2Icon color='gray.300'/>} />
+  </InputGroup>
+
+        
+        
+        
+        
+        
+        </Box>
             
-            <Pagination
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* <Pagination
                    postsPerPage={postsPerPage}
                    totalPosts={posts.length}
                    paginate={paginate}
-                 />
+                 /> */}
            
            
            
